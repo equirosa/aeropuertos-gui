9dcfe960782d8842c8d33409f9a52ebec2f6a0fa
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-08-07 14:13:30 -0600

    Cambiar indentacion.

--- src/com/eduardoquiros/ui/Main.java
+++ src/com/eduardoquiros/ui/Main.java
@@ -5,10 +5,21 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
+        desplegarLogin();
+    }
+
+    private static void desplegarLogin() {
         JFrame frame = new JFrame("Login");
         frame.setContentPane(new LoginForm().getLoginPanel());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        desplegarLogin();
     }
+    
+    private static void obtenerDayos(){
+    
+    }
+
+
 }
